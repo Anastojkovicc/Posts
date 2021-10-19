e6c00bb423607a3c5a7012bf90f2ad7f53736f1c
Ana Stojkovic <anastojkovic@sbgs-MacBook-Pro-3.local>
2021-10-19 12:39:54 +0200

    Kreirana forma za unos novog posta

--- Posts/src/app/app.module.ts
+++ Posts/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts-component/posts-component.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
+    PostsComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ Posts/src/app/post-form/post-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output,EventEmitter, ViewChild} from '@angular/core';
+import { Post } from '../post';
+import { PostService} from '../services/post-service.service';
+
+
+@Component({
+  selector: 'app-post-form',
+  templateUrl: './post-form.component.html',
+  styleUrls: ['./post-form.component.css']
+})
+
+export class PostFormComponent implements OnInit {
+  
+  public postInformations: Post = {
+    title: '',
+    body: ''
+  };
+
+  constructor(private postService:PostService) { }
+
+  ngOnInit(): void {
+  }
+
+}
